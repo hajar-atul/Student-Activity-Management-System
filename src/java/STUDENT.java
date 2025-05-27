5748e7247ffb68c630e2b8582ed5a36a4672109d
User <User@10.62.48.175>
2025-05-28 00:46:32 +0800

    TEST

---
+++ src/java/STUDENT.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class STUDENT {
+    private String studId;
+    private String studName;
+    private String studEmail;
+    private String studProgram;
+    private String studSemester;
+    private int studNoPhone;
+    private String studType;
+    private String studPassword;
+
+    // Setter and Getter for studId
+    public void setStudId(String studId) {
+        this.studId = studId;
+    }
+
+    public String getStudId() {
+        return studId;
+    }
+
+    // Setter and Getter for studName
+    public void setStudName(String studName) {
+        this.studName = studName;
+    }
+
+    public String getStudName() {
+        return studName;
+    }
+
+    // Setter and Getter for studEmail
+    public void setStudEmail(String studEmail) {
+        this.studEmail = studEmail;
+    }
+
+    public String getStudEmail() {
+        return studEmail;
+    }
+
+    // Setter and Getter for studProgram
+    public void setStudProgram(String studProgram) {
+        this.studProgram = studProgram;
+    }
+
+    public String getStudProgram() {
+        return studProgram;
+    }
+
+    // Setter and Getter for studSemester
+    public void setStudSemester(String studSemester) {
+        this.studSemester = studSemester;
+    }
+
+    public String getStudSemester() {
+        return studSemester;
+    }
+
+    // Setter and Getter for studNoPhone
+    public void setStudNoPhone(int studNoPhone) {
+        this.studNoPhone = studNoPhone;
+    }
+
+    public int getStudNoPhone() {
+        return studNoPhone;
+    }
+
+    // Setter and Getter for studType
+    public void setStudType(String studType) {
+        this.studType = studType;
+    }
+
+    public String getStudType() {
+        return studType;
+    }
+
+    // Setter and Getter for studPassword
+    public void setStudPassword(String studPassword) {
+        this.studPassword = studPassword;
+    }
+
+    public String getStudPassword() {
+        return studPassword;
+    }
+}
+
